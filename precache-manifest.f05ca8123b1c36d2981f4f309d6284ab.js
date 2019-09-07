f9f02f45a54dda170e72c3fe4ba53f39bdd7f4c4
bk107 <bastian.kaltdorf@gmail.com>
2019-09-07 10:44:21 +0200

    Updates

---
+++ precache-manifest.f05ca8123b1c36d2981f4f309d6284ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "383e19ccc6c44f05f50712e227a40f85",
+    "url": "/marketplace/index.html"
+  },
+  {
+    "revision": "c611262392e1247a1e11",
+    "url": "/marketplace/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "303be5b4bd535e923669",
+    "url": "/marketplace/static/js/2.eb70a074.chunk.js"
+  },
+  {
+    "revision": "c611262392e1247a1e11",
+    "url": "/marketplace/static/js/main.6dca8c29.chunk.js"
+  },
+  {
+    "revision": "7c09ef1129ca022e98e5",
+    "url": "/marketplace/static/js/runtime~main.4f5b03ec.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/marketplace/static/media/logo.5d5d9eef.svg"
+  }
+]);
